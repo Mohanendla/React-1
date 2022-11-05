293faae8f2f88df02ffe344af9dc33688bad239f
Mohanendla <endla>
2022-11-05 15:34:17 +0530

    React-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Component1 from "./Component1";
+import Component2 from "./Component2"
+import Component3 from "./Component3";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="page-title"> My First React App </h1>
+      <Component1/>
+     <Component3></Component3>
+      <Component2/>
+      <Component2/>
+      <Component1/>
+      <Component1/>
+      <Component1/>
     </div>
   );
 }
 
 export default App;
+
+/*
+REACT
+ -SPA (Single Page Application)
+ - Uses Virtual DOM
+
+  Delcaration Library
+  - Tell react WHAT to do
+  - Don't tell react HOW to do, react handles it
+
+  JSX: JavaScript + XML
+
+  Components - (WHICH RETURNS JSX)
+  Two types:
+  - Class Based components (JavaScript Classes)
+  - Function Based components (JavaScript Functions)
+  Properties:
+    - Create parts of UI
+    - Can be nested
+    - Can be re-used
+  */
